bafaa902177c873f1cf1b85084e7db0a2faf3bbb
heather propes <hbuch4@yahoo.com>
2025-06-16 14:37:05 -0400

    comments now eager loading in posting. yay.

--- app/models/postingschema.py
+++ app/models/postingschema.py
@@ -1,8 +1,13 @@
+from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from app.models.commentschema import CommentSchema
 from app.models.posting import Posting
 
 
 class PostingSchema(SQLAlchemyAutoSchema):
+    comments = fields.Nested(CommentSchema, many=True)
+
     class Meta:
         model = Posting
         include_relationships = True
